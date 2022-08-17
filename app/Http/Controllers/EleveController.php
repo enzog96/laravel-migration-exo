6aeaf5f24b41a535ae5baf9aaadf962febd7f72c
enzo grillo <enzogrillo96@gmail.com>
2022-08-17 16:01:49 +0200

    first commit

---
+++ app/Http/Controllers/EleveController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\eleve;
+use App\Http\Requests\StoreeleveRequest;
+use App\Http\Requests\UpdateeleveRequest;
+
+class EleveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreeleveRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreeleveRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\eleve  $eleve
+     * @return \Illuminate\Http\Response
+     */
+    public function show(eleve $eleve)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\eleve  $eleve
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(eleve $eleve)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateeleveRequest  $request
+     * @param  \App\Models\eleve  $eleve
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateeleveRequest $request, eleve $eleve)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\eleve  $eleve
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(eleve $eleve)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_08_17_135608_create_eleves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eleves', function (Blueprint $table) {
+            $table->id();
+            $table->string("nom", 50);
+            $table->string("prenom", 50);
+            $table->string("email", 50);
+            $table->string("address", 200);
+            $table->date("date");
+            $table->ipAddress("ip");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eleves');
+    }
+};
